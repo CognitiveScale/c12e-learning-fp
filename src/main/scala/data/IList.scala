11ca541fd4cea28f3704813b3204dca4da531e5a
Mayank Asthana <masthana@cognitivescale.com>
2017-06-05 18:57:35 -0500

    Implement IList methods

--- src/main/scala/data/IList.scala
+++ src/main/scala/data/IList.scala
@@ -14,6 +14,37 @@ sealed trait IList[A] {
       case Cons(h, t) => ifCons(h, t.fold(ifNil)(ifCons))
     }
 
+  def foldLeft[B](initAcc: B)(f: (B, A) => B): B = this match {
+    case Nil() => initAcc
+    case Cons(h, t) => f(t.foldLeft(initAcc)(f), h)
+  }
+
+
+  def map[B](f: A => B): IList[B] = fold(nil[B]) {(head: A, acc: IList[B]) => Cons(f(head), acc)}
+
+
+  def flatMap[B](f: A => IList[B]): IList[B] = fold(nil[B]) { (h: A, t: IList[B]) => f(h) extend t }
+
+
+  def reverse: IList[A] = foldLeft(nil[A])(_.append(_))
+
+  def reverse1: IList[A] = this match {
+    case Nil() => nil
+    case Cons(head, tail) => tail.reverse extend cons(head, nil)
+  }
+
+
+  def extend(other: IList[A]): IList[A] = fold(other)(Cons(_,_))
+
+  def extend1(other: IList[A]): IList[A] = this match {
+    case Nil() => other
+    case Cons(h, t) => Cons(h,t extend1 other)
+  }
+
+
+  def append(a: A): IList[A] = foldLeft(Cons(a, nil))((t, h) => Cons(h, t))
+
+  def append1(a: A): IList[A] = Cons(a, this.reverse).reverse
 }
 
 final case class Nil[A]() extends IList[A]
@@ -29,5 +60,23 @@ object IList {
 object IListTest extends App {
   val testList: IList[Int] = cons(1, cons(2, cons(3, nil)))
   println(testList)
-  println(testList.fold(0) {_ + _ })
+
+  println(s"Sum using fold of $testList: ${testList.fold(0) {_ + _ }}")
+  assert((testList.fold(0) {_ + _ }).equals(6), "fold fail")
+
+  println(s"Reverse $testList: ${testList.reverse}")
+  assert(testList.reverse.equals(cons(3, cons(2, cons(1, nil)))), "Reverse fail")
+
+  val flatMapRes: IList[Int] = testList.flatMap { i => Cons(i+1, Cons(i+2, nil))}
+  assert(flatMapRes.equals(Cons(2, Cons(3, Cons(3, Cons(4, Cons(4, Cons(5, nil))))))), "flatMap fail")
+
+  val mapRes: IList[String] = testList.map(_.toString)
+  assert(mapRes.equals(Cons("1", Cons("2", Cons("3", nil)))), "map fail")
+
+  println(s"Extend list: $testList + $testList = ${testList extend testList}")
+  assert((testList extend testList).equals(Cons(1, Cons(2, Cons(3, Cons(1, Cons(2, Cons(3, nil))))))), "Extend list fail")
+
+  assert((testList append 10).equals(Cons(1, Cons(2, Cons(3, Cons(10, nil))))), "Append to list fail")
+  println(s"Append 10 to list $testList: ${testList append 10}")
+
 }
