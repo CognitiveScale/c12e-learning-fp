2f72dd75178ec0445e5d80ba317aae5bef6cdad5
Christine Draper <cdraper@cognitivescale.com>
2017-07-22 15:29:27 -0500

    last and max

---
+++ src/main/scala/data/Last.scala
@@ -0,0 +1,19 @@
+package com.c12e.learn
+package data
+
+import com.c12e.learn.typeclass.{Equal, Semigroup}
+
+
+final case class Last[A](value: A) extends AnyVal
+
+object Last {
+
+  implicit def equal[A : Equal]: Equal[Last[A]] =
+    Equal[A] contramap { _.value }
+
+  implicit def semigroupLastInt: Semigroup[Last[Int]] = {
+    new Semigroup[Last[Int]] {
+      def append(x: Last[Int], y: Last[Int]) = y
+    }
+  }
+}
