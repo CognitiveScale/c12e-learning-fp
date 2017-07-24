b32ed2a683d9d25c9a1ab2a29e3cf446016597d0
Christine Draper <cdraper@cognitivescale.com>
2017-07-24 10:03:44 -0500

    Fix plus tests to use our Equal

--- src/main/scala/typeclass/Plus.scala
+++ src/main/scala/typeclass/Plus.scala
@@ -50,30 +50,30 @@ object Plus {
 object TestPlus extends App {
   import Syntax._
 
-  import scalaz.std.option._
-  import scalaz.std.anyVal.intInstance
-  import scalaz.std.list._
-  import scalaz.syntax.equal._
+//  import scalaz.std.option._
+//  import scalaz.std.anyVal.intInstance
+//  import scalaz.std.list._
+//  import scalaz.syntax.equal._
 
-//  import Equal.Syntax._
-//  private val none : Option[Int] = None
-//  implicit def myIntEqual: Equal[Int] = Equal.fromObject[Int]
-//  implicit def myOptionEqual[A: Equal]: Equal[Option[A]] = new Equal[Option[A]] {
-//    override def equal(a1: Option[A], a2: Option[A])(implicit ev: Equal[A]): Boolean =
-//      (a1,a2) match {
-//        case (None,None) => true
-//        case (Some(aa1),Some(aa2)) => (aa1 === aa2)
-//        case _ => false
-//      }
-//  }
-//  implicit def myListEqual[A: Equal]: Equal[List[A]] = new Equal[List[A]] {
-//    override def equal(l1: List[A], l2: List[A])(implicit ev: Equal[A]): Boolean =
-//      (l1, l2) match {
-//        case (Nil,Nil) => true
-//        case (h1 :: tail1, h2 :: tail2) => h1 === h2 && tail1 === tail2
-//        case _ => false
-//      }
-//  }
+  import Equal.Syntax._
+  private def none[A] : Option[A] = None
+  implicit def myIntEqual: Equal[Int] = Equal.fromObject[Int]
+  implicit def myOptionEqual[A: Equal]: Equal[Option[A]] = new Equal[Option[A]] {
+    def equal(a1: Option[A], a2: Option[A]): Boolean =
+      (a1,a2) match {
+        case (None,None) => true
+        case (Some(aa1),Some(aa2)) => (aa1 === aa2)
+        case _ => false
+      }
+  }
+  implicit def myListEqual[A: Equal]: Equal[List[A]] = new Equal[List[A]] {
+    def equal(l1: List[A], l2: List[A]): Boolean =
+      (l1, l2) match {
+        case (Nil,Nil) => true
+        case (h1 :: tail1, h2 :: tail2) => h1 === h2 && tail1 === tail2
+        case _ => false
+      }
+  }
 
   assert((List(1, 2, 3) <+> List(4, 5, 6)) === List(1, 2, 3, 4, 5, 6))
   assert((Option(1) <+> Option(2)) === Option(1))
