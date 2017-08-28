667edbea879cb664c1632a81247de91ac0284f25
Christine Draper <cdraper@cognitivescale.com>
2017-08-28 09:42:37 -0500

    Convert to non-curried fns

--- src/main/scala/data/ScottMaybe.scala
+++ src/main/scala/data/ScottMaybe.scala
@@ -6,20 +6,19 @@ import Equal.Syntax._
 sealed trait ScottMaybe[A] {
   import ScottMaybe.{just,nothing}
 
-  // is this still scott encoding, or does it have to be (c1, c2, c3...) => B?
-  def matchS[B]: B => (A => B) => B
+  def matchS[B]: (B, A => B) => B
 
-  def fold[B]: B => (A => B) => B = matchS
+  def fold[B]: (B, A => B) => B = matchS
 
-  def getOrElse: A => A = matchS(_)(identity[A])
+  def getOrElse: A => A = matchS(_, identity[A])
 
-  def map[B]: (A => B) => ScottMaybe[B] = f => matchS(nothing[B])(just compose f)
+  def map[B]: (A => B) => ScottMaybe[B] = f => matchS(nothing[B], just compose f)
 
-  def flatMap[B]: (A => ScottMaybe[B]) => ScottMaybe[B] = matchS(nothing[B])(_)
+  def flatMap[B]: (A => ScottMaybe[B]) => ScottMaybe[B] = matchS(nothing[B], _)
 
-  def isNothing: Boolean = matchS(true)(_ => false)
+  def isNothing: Boolean = matchS(true, _ => false)
 
-  def filter: (A => Boolean) => ScottMaybe[A] = f => matchS(nothing[A])(a => if(f(a)) this else nothing[A])
+  def filter: (A => Boolean) => ScottMaybe[A] = f => matchS(nothing[A], a => if(f(a)) this else nothing)
   //def filter: (A => Boolean) => ScottMaybe[A] = f => map(f).flatMap(b => if(b) this else nothing)
 
 }
@@ -29,14 +28,14 @@ object ScottMaybe {
   implicit def intEqual: Equal[Int] = Equal.fromObject[Int]
   implicit def scottMaybeEqual[A: Equal]: Equal[ScottMaybe[A]] = new Equal[ScottMaybe[A]] {
     def equal(m1: ScottMaybe[A], m2: ScottMaybe[A]): Boolean =
-      m1.matchS(m2.isNothing)(a1 => m2.matchS(false)(_ === a1))
+      m1.matchS(m2.isNothing, a1 => m2.matchS(false, _ === a1))
   }
 
   def nothing[A]: ScottMaybe[A] = new ScottMaybe[A] {
-    def matchS[B]: B => (A => B) => B = b => _ => b
+    def matchS[B]: (B, A => B) => B = (b, _) => b
   }
   def just[A]: A => ScottMaybe[A] = a => new ScottMaybe[A] {
-    def matchS[B]: B => (A => B) => B = _ => f => f(a)
+    def matchS[B]: (B, A => B) => B = (_, f) => f(a)
   }
 
 }
@@ -55,8 +54,8 @@ object TestPlus extends App {
   assert(just(1).getOrElse(3) === 1)
   assert(nothing.getOrElse(3) === 3)
 
-  assert(just(1).fold(5)(_ * 2) === 2)
-  assert(nothing[Int].fold(5)(_ * 2) === 5)
+  assert(just(1).fold(5, _ * 2) === 2)
+  assert(nothing[Int].fold(5, _ * 2) === 5)
 
   assert(just(1).map(_ * 2) === just(2))
   assert(nothing[Int].map(_ * 2) === nothing)
