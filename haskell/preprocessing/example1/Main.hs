18fa9ac8a8010e928a95b9fe424e5b42236bc765
sbhat-cs <sbhat@cognitivescale.com>
2018-04-12 18:36:02 -0500

    haskell/preprocessing: add homework 1

--- haskell/preprocessing/example1/Main.hs
+++ haskell/preprocessing/example1/Main.hs
@@ -1,40 +1,46 @@
+-- executables: the module name *must* be "Main" (!)
+
 module Main where
 
-import Lib
 
 main :: IO ()
-{-main = putStrLn "Hello Mars!"-}
-{-main = echo-}
+-- main = putStrLn "Hello Mars!"
+-- main = echo
 main = echoQuit
 
 
 echo :: IO ()
 echo = getLine >>= putStrLn
 
+
 echoServer :: IO ()
 echoServer = getLine >>= (\s -> putStrLn s >>= (\_ -> echoServer) )
+
 
 echoServer2 :: IO ()
 echoServer2 = (getLine >>= putStrLn) >>= (\_ -> echoServer2)
 
+
 echoServer3 :: IO ()
 echoServer3 = (getLine >>= putStrLn) >> echoServer3
+
 
 echoServer4 :: IO ()
 echoServer4 = echo >> echoServer4
 
+
 echoQuit :: IO ()
 echoQuit = getLine >>= (\s ->
     if s == "quit" then
-        {-else putStrLn s >>= (\_ -> echoServer)-}
         pure ()
     else
         putStrLn s >>= (\_ -> echoQuit)
     )
+
 
 {-
  (>>=) :: IO a -> (a -> IO b) -> IO b
 -}
 
 (>>>) :: Monad m => m a -> m b -> m b
-(>>>) a b = a >>= (\_ -> b) 
+(>>>) a b = a >>= (\_ -> b)
---
+++ haskell/preprocessing/homework1/Main.hs
@@ -0,0 +1,48 @@
+-- executables: the module name *must* be "Main" (!)
+
+module Main where
+
+import System.IO (isEOF)
+
+
+{----------------------------------------------------------------
+
+Homework: write a program that reads lines of numbers from stdin and
+writes back those lines, including their sums, to stdout.
+
+For example,
+
+$ cat data/numbers.txt
+1 2 3
+4 5 6
+
+$ stack exec homework1 < data/numbers.txt
+1 2 3 6
+4 5 6 15
+
+
+For convenience, assume space-delimited input/output.  You'll find the
+"words" and "unwords" functions handy.
+
+You'll need to use "isEOF" before any call to "getLine" to check for
+the end of the input.
+
+----------------------------------------------------------------}
+
+
+main :: IO ()
+main =
+    processStdin
+
+
+processStdin :: IO ()
+processStdin =
+    error "processStdin: not implemented"
+
+
+-- Yes, this is partial function.
+-- Yes, partial functions are bad.
+-- Yes, we will eventually fix this.
+--
+readInt :: String -> Int
+readInt = read
